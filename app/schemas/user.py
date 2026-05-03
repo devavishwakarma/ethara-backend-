921ee6715b27b177076ef6b3a06a1a5e5f52e63d
devavishwakarma <157609704+devavishwakarma@users.noreply.github.com>
2026-05-04 00:14:34 +0530

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,9 +4,4 @@ class UserCreate(BaseModel):
     name: str
     email: str
     password: str
-
-class UserCreate(BaseModel):
-    name: str
-    email: str
-    password: str
     role: str = "member"    
