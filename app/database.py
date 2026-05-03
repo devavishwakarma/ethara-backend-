19227feea6c104fff3b47edac587e610d1fbfdad
devavishwakarma <157609704+devavishwakarma@users.noreply.github.com>
2026-05-03 16:51:38 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -4,8 +4,6 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-# Force correct .env path
-env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
